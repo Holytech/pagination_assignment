bd64217b870979b9739439b14e2080d7c19ab229
Akeem Qudus I <akeemqudus2016@gmail.com>
2022-10-27 23:30:47 +0100

    Update useFetch.jsx

--- src/components/useFetch.jsx
+++ src/components/useFetch.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(false);
